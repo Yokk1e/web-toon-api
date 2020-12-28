03bf4b21090323ffa2320309a82258c293e85805
Yok-1337 <satthabut.o@gmail.com>
2020-12-29 00:59:04 +0700

    - [feat] update

--- src/managed-entities/managed-entities.module.ts
+++ src/managed-entities/managed-entities.module.ts
@@ -1,4 +1,5 @@
-import { Module } from '@nestjs/common';
+import { Module, Global } from '@nestjs/common';
 
+@Global()
 @Module({})
 export class ManagedEntitiesModule {}
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { UsersController } from './users/users.controller';
 import { UsersService } from './users/users.service';
+import { User } from './users/user.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([User])],
   controllers: [UsersController],
-  providers: [UsersService]
+  providers: [UsersService],
 })
 export class UsersModule {}
