d8fa026133f434760929f4ef577eb044bb7b8773
Yok-1337 <satthabut.o@gmail.com>
2020-12-31 03:49:42 +0700

    - [feat] update create user dto

---
+++ src/users/users/dto/create-user.dto.ts
@@ -0,0 +1,38 @@
+import { IsNotEmpty, Length, IsInt, MinLength, IsEnum } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+
+import { GenderType } from '../user.entity';
+
+export class CreateUserDto {
+  @IsNotEmpty()
+  @Length(1, 255)
+  @ApiProperty()
+  readonly email: string;
+
+  @IsNotEmpty()
+  @Length(1, 255)
+  @MinLength(6, {
+    message: 'Password must have at least 6 letters.',
+  })
+  @ApiProperty()
+  readonly password: string;
+
+  @IsNotEmpty()
+  @Length(1, 255)
+  @ApiProperty()
+  readonly userName: string;
+
+  @IsNotEmpty()
+  @IsEnum(GenderType)
+  @Length(1, 255)
+  @ApiProperty({
+    type: 'enum',
+    enum: GenderType,
+  })
+  readonly gender: GenderType;
+
+  @IsNotEmpty()
+  @IsInt()
+  @ApiProperty()
+  readonly age: number;
+}
