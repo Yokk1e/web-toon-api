f4c291d818e1fd09a503ef794d5e443ba76ec1d6
Yok-1337 <satthabut.o@gmail.com>
2020-12-31 13:09:45 +0700

    - [feat] update main

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { ConfigService } from '@nestjs/config';
 import { AppModule } from './app.module';
@@ -14,6 +15,15 @@ async function bootstrap() {
     .setVersion('1.0')
     .addTag('Webtoon')
     .build();
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      disableErrorMessages: false,
+      whitelist: true,
+      transform: true,
+    }),
+  );
+  
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('api-docs', app, document);
 
