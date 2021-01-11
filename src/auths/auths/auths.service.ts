fc72de79a224688529a7d74569d40d7bd2be28df
Yok-1337 <satthabut.o@gmail.com>
2021-01-11 10:40:27 +0700

    - [feat] add service validate user

--- src/auths/auths/auths.service.ts
+++ src/auths/auths/auths.service.ts
@@ -1,4 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import { compare } from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
+
+import { UsersService } from '../../users/users/users.service';
 
 @Injectable()
-export class AuthsService {}
+export class AuthsService {
+    constructor(
+        private readonly usersService: UsersService,
+        private readonly jwtService: JwtService,
+      ) {}
+
+      async validateUser(email: string, pass: string): Promise<any> {
+        const user = await this.usersService.findByEmail(email);
+    
+        if (user && (await compare(pass, user.password))) {
+          const { password, ...result } = user;
+    
+          return result;
+        }
+    
+        return null;
+      }
+}
