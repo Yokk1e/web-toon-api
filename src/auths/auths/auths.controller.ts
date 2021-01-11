c14ed71dcb4122b6d2e122dce6f8113f4b64ab70
Yok-1337 <satthabut.o@gmail.com>
2021-01-11 10:52:04 +0700

    - [feat] add end point

--- src/auths/auths/auths.controller.ts
+++ src/auths/auths/auths.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Controller, UseGuards, Post, Request } from '@nestjs/common';
+import { ApiTags, ApiBody } from '@nestjs/swagger';
 
+import { AuthsService } from './auths.service';
+import { LocalAuthGuard } from '../locals/local-auth.guard';
+import { LoginDto } from './dto/login.dto';
+
+@ApiTags('auths')
 @Controller('auths')
-export class AuthsController {}
+export class AuthsController {
+  constructor(private readonly authsService: AuthsService) {}
+
+  @ApiBody({ type: LoginDto })
+  @UseGuards(LocalAuthGuard)
+  @Post('login')
+  async login(@Request() req) {
+    return this.authsService.login(req.user);
+  }
+}
