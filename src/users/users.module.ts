ee78bad12f39ef03d82e97071cf4cbc19c6b0ae0
Yok-1337 <satthabut@hocco.co>
2021-03-04 16:31:44 +0700

    - [fix] auth

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -9,5 +9,6 @@ import { User } from './users/user.entity';
   imports: [TypeOrmModule.forFeature([User])],
   controllers: [UsersController],
   providers: [UsersService],
+  exports: [UsersService],
 })
 export class UsersModule {}
