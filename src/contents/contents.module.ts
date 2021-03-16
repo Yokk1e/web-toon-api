380b24e110e281c0360cf925b020497b9bb6aabd
Yokk1e <satthabut.o@gmail.com>
2021-03-17 03:23:35 +0700

    - [feat] add episode module

--- src/contents/contents.module.ts
+++ src/contents/contents.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ContentsService } from './contents/contents.service';
 import { ContentsController } from './contents/contents.controller';
+import { EpisodesController } from './episodes/episodes.controller';
+import { EpisodesService } from './episodes/episodes.service';
 
 @Module({
-  providers: [ContentsService],
-  controllers: [ContentsController]
+  providers: [ContentsService, EpisodesService],
+  controllers: [ContentsController, EpisodesController]
 })
 export class ContentsModule {}
