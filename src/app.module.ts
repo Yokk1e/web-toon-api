96a0633a969e85e20c3d84a365201adf2f0a3d52
Yokk1e <satthabut.o@gmail.com>
2021-03-17 03:11:19 +0700

    - [feat] update app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,11 @@ import { ConfigModule } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
 import { ManagedEntitiesModule } from './managed-entities/managed-entities.module';
 import { AuthsModule } from './auths/auths.module';
+import { ContentsModule } from './contents/contents.module';
+import { ShopsModule } from './shops/shops.module';
+import { ItemsService } from './items/items/items.service';
+import { ItemsController } from './items/items/items.controller';
+import { ItemsModule } from './items/items.module';
 
 @Module({
   imports: [
@@ -13,6 +18,11 @@ import { AuthsModule } from './auths/auths.module';
     ManagedEntitiesModule,
     UsersModule,
     AuthsModule,
+    ContentsModule,
+    ShopsModule,
+    ItemsModule,
   ],
+  providers: [ItemsService],
+  controllers: [ItemsController],
 })
 export class AppModule {}
