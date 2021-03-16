be865a08f5dd487028628d7bfc27ceb7b2b7b886
Yokk1e <satthabut.o@gmail.com>
2021-03-17 04:09:40 +0700

    - [feat] update user module and contents modules

--- src/contents/contents.module.ts
+++ src/contents/contents.module.ts
@@ -1,11 +1,16 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { ContentsService } from './contents/contents.service';
 import { ContentsController } from './contents/contents.controller';
 import { EpisodesController } from './episodes/episodes.controller';
 import { EpisodesService } from './episodes/episodes.service';
+import { Content } from './contents/content.entity';
+import { Episode } from './episodes/episode.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Content, Episode])],
   providers: [ContentsService, EpisodesService],
-  controllers: [ContentsController, EpisodesController]
+  controllers: [ContentsController, EpisodesController],
 })
 export class ContentsModule {}
