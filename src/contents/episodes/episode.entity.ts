e7d83e439e0e77c2ca7f382fbafa2e9e11f4530b
Yokk1e <satthabut.o@gmail.com>
2021-03-17 04:09:16 +0700

    - [feat] add content and episode entity

---
+++ src/contents/episodes/episode.entity.ts
@@ -0,0 +1,24 @@
+import { Entity, Column, ManyToOne } from 'typeorm';
+import { Content } from '../contents/content.entity';
+import { ManagedEntity } from '../../managed-entities/managed-entities/managed-entity';
+
+@Entity()
+export class Episode extends ManagedEntity {
+  @Column({ length: 255 })
+  title: string;
+
+  @Column({ length: 255, nullable: true })
+  name: string;
+
+  @Column({ type: 'text', nullable: true })
+  description: string;
+
+  @Column({ type: 'text' })
+  link: string;
+
+  @ManyToOne(
+    type => Content,
+    content => content.episodes,
+  )
+  content: Content;
+}
