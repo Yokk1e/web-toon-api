580e348a68bc4b570fd9f5970e44e4b22ff22054
Yokk1e <satthabut.o@gmail.com>
2021-03-18 16:42:30 +0700

    - [feat] add query dto

---
+++ src/managed-entities/managed-entities/dto/managed-query.dto.ts
@@ -0,0 +1,59 @@
+import { IsOptional, IsBoolean, Min, IsNumber, IsEnum } from 'class-validator';
+import { Type, Transform } from 'class-transformer';
+import { ApiPropertyOptional } from '@nestjs/swagger';
+
+export enum OrderType {
+  DESC = 'DESC',
+  ASC = 'ASC',
+}
+
+export class ManagedQueryDto {
+  @IsOptional()
+  @IsBoolean()
+  @Type(() => Boolean)
+  @ApiPropertyOptional()
+  readonly deleted: boolean;
+
+  @IsOptional()
+  @IsNumber()
+  @Min(1)
+  @Transform(value => Number(value))
+  @ApiPropertyOptional({ type: Number })
+  readonly page: number = 1;
+
+  @IsOptional()
+  @IsNumber()
+  @Min(1)
+  @Transform(value => Number(value))
+  @ApiPropertyOptional({ type: Number })
+  readonly limit: number = 10;
+
+  @IsOptional()
+  @ApiPropertyOptional({ type: String })
+  readonly key: string = 'id';
+
+  @IsOptional()
+  @IsEnum(OrderType)
+  @ApiPropertyOptional({
+    type: 'enum',
+    enum: OrderType,
+    default: OrderType.ASC,
+  })
+  readonly orderType: OrderType;
+
+  @IsOptional()
+  @ApiPropertyOptional()
+  readonly search: string;
+
+  public toWhereClause(): any {
+    return this.deleted ? {} : { active: true };
+  }
+
+  public orderBy(): any {
+    return { [this.key]: this.orderType };
+  }
+
+  public orderByIdDesc(): any {
+    return { id: 'DESC' };
+  }
+}
