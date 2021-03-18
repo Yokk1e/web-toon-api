fe2eb9861a14ac85af992738ebaeced0cac8f043
Yokk1e <satthabut.o@gmail.com>
2021-03-18 16:25:52 +0700

    - [style] format

--- src/contents/contents/dto/update-content.dto.ts
+++ src/contents/contents/dto/update-content.dto.ts
@@ -1,31 +1,30 @@
 import {
-    IsNotEmpty,
-    Length,
-    IsString,
-    ValidateNested,
-    IsOptional,
-  } from 'class-validator';
-  import { Type } from 'class-transformer';
-  import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
-  
-  import { ManagedLogDto } from 'src/managed-entities/managed-entities/dto/managed-log.dto';
-  import { UpdateEpisodeDto } from '../../episodes/dto/update-episode.dto';
-  
-  export class UpdateContentDto extends ManagedLogDto {
-    @IsOptional()
-    @Length(1, 255)
-    @ApiProperty()
-    readonly name: string;
-  
-    @IsOptional()
-    @IsString()
-    @ApiProperty()
-    readonly description: string;
-  
-    @IsOptional()
-    @ValidateNested({ each: true })
-    @Type(() => UpdateEpisodeDto)
-    @ApiPropertyOptional({ type: [UpdateEpisodeDto] })
-    readonly episodes: UpdateEpisodeDto[];
-  }
-  
+  IsNotEmpty,
+  Length,
+  IsString,
+  ValidateNested,
+  IsOptional,
+} from 'class-validator';
+import { Type } from 'class-transformer';
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+
+import { ManagedLogDto } from 'src/managed-entities/managed-entities/dto/managed-log.dto';
+import { UpdateEpisodeDto } from '../../episodes/dto/update-episode.dto';
+
+export class UpdateContentDto extends ManagedLogDto {
+  @IsOptional()
+  @Length(1, 255)
+  @ApiProperty()
+  readonly name: string;
+
+  @IsOptional()
+  @IsString()
+  @ApiProperty()
+  readonly description: string;
+
+  @IsOptional()
+  @ValidateNested({ each: true })
+  @Type(() => UpdateEpisodeDto)
+  @ApiPropertyOptional({ type: [UpdateEpisodeDto] })
+  readonly episodes: UpdateEpisodeDto[];
+}
