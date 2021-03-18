8e92f673d3ff46ff20b6157d84dd3f17081375a0
Yokk1e <satthabut.o@gmail.com>
2021-03-18 16:42:59 +0700

    - [feat] update users service

--- src/users/users/users.controller.ts
+++ src/users/users/users.controller.ts
@@ -1,10 +1,29 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import {
+  Controller,
+  UseGuards,
+  Post,
+  Body,
+  Get,
+  Header,
+  Query,
+  UseInterceptors,
+  ClassSerializerInterceptor,
+  Delete,
+  Param,
+  Patch,
+} from '@nestjs/common';
 import { ApiTags, ApiBearerAuth } from '@nestjs/swagger';
+import { JwtAuthGuard } from '../../auths/jwts/jwt-auth.guard';
 import { UsersService } from './users.service';
 
 import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { UserQueryDto } from './dto/user-query.dto';
 
 @ApiTags('users')
+@ApiBearerAuth()
+@UseGuards(JwtAuthGuard)
+@UseInterceptors(ClassSerializerInterceptor)
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
@@ -12,5 +31,29 @@ export class UsersController {
   @Post()
   create(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
+  }
+
+  @Get()
+  @Header('Cache-Control', 'no-cache, no-store')
+  findAll(@Query() query: UserQueryDto) {
+    const options = { limit: query.limit, page: query.page };
+
+    return this.usersService.findAll(query, options);
+  }
+
+  @Get(':id')
+  @Header('Cache-Control', 'no-cache, no-store')
+  findOne(@Param('id') id: number, @Query() query: UserQueryDto) {
+    return this.usersService.findOne(id, query);
+  }
+
+  @Patch(':id')
+  updateOne(@Param('id') id: number, @Body() updateRoleDto: UpdateUserDto) {
+    return this.usersService.updateOne(id, updateRoleDto);
+  }
+
+  @Delete(':id')
+  deleteOne(@Param('id') id: number) {
+    return this.usersService.deleteOne(id);
   }
 }
