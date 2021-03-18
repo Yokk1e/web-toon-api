0f2defe1966b992a639e6ac1d8682a89a5c706e1
Yokk1e <satthabut.o@gmail.com>
2021-03-18 15:59:07 +0700

    - [feat] add create content service

--- src/contents/contents/contents.controller.ts
+++ src/contents/contents/contents.controller.ts
@@ -1,4 +1,36 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  UseGuards,
+  Post,
+  Body,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { ApiTags, ApiBearerAuth } from '@nestjs/swagger';
 
+import { GetJwtUser } from '../../auths/jwts/decorator/get-jwt-user.decorator';
+import { JwtAuthGuard } from '../../auths/jwts/jwt-auth.guard';
+import { JwtUser } from '../../auths/jwts/jwt.strategy';
+
+import { ContentsService } from './contents.service';
+import { CreateContentDto } from './dto/create-content.dto';
+
+@ApiTags('contents')
+@ApiBearerAuth()
+@UseGuards(JwtAuthGuard)
 @Controller('contents')
-export class ContentsController {}
+export class ContentsController {
+  constructor(private readonly contentsService: ContentsService) {}
+
+  @Post()
+  @UseInterceptors(FileInterceptor('file'))
+  create(
+    @UploadedFile() file,
+    @Body() createContentDto: CreateContentDto,
+    @GetJwtUser() user: JwtUser,
+  ) {
+    createContentDto.logUserCreate(user.userId, user.userName);
+    return this.contentsService.create(createContentDto);
+  }
+}
--- src/contents/contents/contents.service.ts
+++ src/contents/contents/contents.service.ts
@@ -1,4 +1,33 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, HttpException, HttpStatus, Inject } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
+import { Content } from './content.entity';
+
+import { CreateContentDto } from './dto/create-content.dto';
 
 @Injectable()
-export class ContentsService {}
+export class ContentsService {
+  constructor(
+    @InjectRepository(Content)
+    private readonly contentRepository: Repository<Content>,
+  ) {}
+
+  async create(createContentDto: CreateContentDto): Promise<Content> {
+    const content = await this.contentRepository.findOne({
+      where: {
+        name: createContentDto.name,
+      },
+    });
+
+    if (content) {
+      throw new HttpException('This name is used', HttpStatus.BAD_REQUEST);
+    }
+
+    return this.contentRepository.save({ ...createContentDto });
+  }
+
+  async findOne(contentId: number) {
+    return this.contentRepository.findOneOrFail(contentId);
+  }
+}
