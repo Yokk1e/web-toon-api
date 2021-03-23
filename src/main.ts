e16617a5835f8a22c21598ef7d0a688b08c146fe
Yokk1e <satthabut.o@gmail.com>
2021-03-23 18:51:37 +0700

    - [feat] update main

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,19 @@ import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { ConfigService } from '@nestjs/config';
+import * as Sentry from '@sentry/node';
 import { AppModule } from './app.module';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const configService = app.get(ConfigService);
   const port = configService.get('NEST_PORT') || 3000;
+
+  Sentry.init({
+    dsn: process.env.SENTRY_DSN_URL,
+  });
 
   const options = new DocumentBuilder()
     .addBearerAuth()
