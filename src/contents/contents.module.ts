3e3bfac0d1eab0396e3a6107598aea5c7690dfbe
Yokk1e <satthabut.o@gmail.com>
2021-03-23 18:14:46 +0700

    - [feat] update content notification when create and update content🥴

--- src/contents/contents.module.ts
+++ src/contents/contents.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-
+import { ConfigService, ConfigModule } from '@nestjs/config';
+import { BullModule } from '@nestjs/bull';
 import { ContentsService } from './contents/contents.service';
 import { ContentsController } from './contents/contents.controller';
 import { EpisodesController } from './episodes/episodes.controller';
@@ -9,7 +10,21 @@ import { Content } from './contents/content.entity';
 import { Episode } from './episodes/episode.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Content, Episode])],
+  imports: [
+    TypeOrmModule.forFeature([Content, Episode]),
+    BullModule.registerQueueAsync({
+      name: 'notifications',
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        redis: {
+          host: configService.get('REDIS_HOST'),
+          port: +configService.get('REDIST_PORT'),
+          password: configService.get('REDIS_PASSWORD'),
+        },
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   providers: [ContentsService, EpisodesService],
   controllers: [ContentsController, EpisodesController],
 })
