7880a3ff88792be622a9f4715b19d406bc93d036
Yok-1337 <satthabut@hocco.co>
2021-03-28 16:05:00 +0700

    - [fix] remove inuse function in role query dto

--- src/users/roles/dto/role-query.dto.ts
+++ src/users/roles/dto/role-query.dto.ts
@@ -1,7 +1,3 @@
 import { ManagedQueryDto } from '../../../managed-entities/managed-entities/dto/managed-query.dto';
 
-export class RoleQueryDto extends ManagedQueryDto {
-  public toWhereClause(): any {
-    return super.toWhereClause();
-  }
-}
+export class RoleQueryDto extends ManagedQueryDto {}
