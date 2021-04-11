4d7065c5af90f07e15ea01cfc12c0e72c89010aa
Pongphol <fook._.004@hotmail.com>
2021-04-11 12:31:59 +0700

    -[feat] add create item

--- src/items/items/items.controller.ts
+++ src/items/items/items.controller.ts
@@ -1,4 +1,29 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  UseGuards,
+  Post,
+  Body,
+  Get,
+  Header,
+  Query,
+  UseInterceptors,
+  ClassSerializerInterceptor,
+  Delete,
+  Param,
+  Patch,
+} from '@nestjs/common';
+import { ApiTags, ApiBearerAuth } from '@nestjs/swagger';
+import { ItemsService } from './items.service';
+import { CreateItemDto } from './dto/create-item.dto';
 
+@ApiTags('items')
 @Controller('items')
-export class ItemsController {}
+export class ItemsController {
+  constructor(private readonly itemsService: ItemsService) {}
+
+  @Post()
+  create(@Body() createItemDto: CreateItemDto) {
+    return this.itemsService.create(createItemDto);
+  }
+
+}
--- src/items/items/items.service.ts
+++ src/items/items/items.service.ts
@@ -1,4 +1,31 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, HttpException, HttpStatus } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Item } from '../items/items.entity';
+import { Content } from '../../contents/contents/content.entity';
+import { CreateItemDto } from './dto/create-item.dto';
 
 @Injectable()
-export class ItemsService {}
+export class ItemsService {
+  constructor(
+    @InjectRepository(Item)
+    private readonly itemRepository: Repository<Item>,
+    @InjectRepository(Content)
+    private readonly contentRepository: Repository<Content>,
+  ) {}
+
+  async create(createItemDto: CreateItemDto): Promise<Item> {
+    const content = await this.contentRepository.findOneOrFail(
+      createItemDto.contentId,
+    );
+
+    const item = await this.itemRepository.findOne({
+      where: { name: createItemDto.name, content },
+    });
+
+    if (item)
+      throw new HttpException('Item is already exist', HttpStatus.BAD_REQUEST);
+
+    return this.itemRepository.save({ ...createItemDto, content });
+  }
+}
