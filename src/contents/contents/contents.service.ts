a51fc7184b172f4ba8da5033389c00e21e834d5f
Yokk1e <satthabut.o@gmail.com>
2021-05-01 02:19:55 +0700

    - [feat] update find all content service

--- src/contents/contents/contents.service.ts
+++ src/contents/contents/contents.service.ts
@@ -3,7 +3,11 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { InjectQueue } from '@nestjs/bull';
 import { Queue } from 'bull';
-
+import {
+  IPaginationOptions,
+  Pagination,
+  paginate,
+} from 'nestjs-typeorm-paginate';
 import { JwtUser } from '../../auths/jwts/jwt.strategy';
 import { Content } from './content.entity';
 import { Episode } from '../episodes/episode.entity';
@@ -17,6 +21,7 @@ import { CreateContentDto } from './dto/create-content.dto';
 import { UpdateContentDto } from './dto/update-content.dto';
 import { CreateEpisodeDto } from '../episodes/dto/create-episode.dto';
 import { UpdateEpisodeDto } from '../episodes/dto/update-episode.dto';
+import { ContentQueryDto } from './dto/content-query.dto';
 
 @Injectable()
 export class ContentsService {
@@ -49,6 +54,22 @@ export class ContentsService {
       ...createContentDto,
       imageFilename: fileName,
     });
+  }
+
+  findAll(
+    query: ContentQueryDto,
+    options: IPaginationOptions,
+  ): Promise<Pagination<Content>> {
+    const contents = this.contentRepository.createQueryBuilder('content');
+    contents.orderBy(`content.${query.key}`, query.orderType);
+
+    if (query.search) {
+      contents.where('content.name like :search', {
+        search: `%${query.search}%`,
+      });
+    }
+
+    return paginate<Content>(contents, options);
   }
 
   async findOne(contentId: number) {
